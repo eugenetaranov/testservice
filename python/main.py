6242781bd7f96a37b3510260cdf3397ad58aa19c
Eugene Taranov <1123643+eugenetaranov@users.noreply.github.com>
2022-11-01 15:48:21 +0300

    added healthcheck toggle

--- python/main.py
+++ python/main.py
@@ -5,6 +5,21 @@ import socket
 
 app = Flask(__name__)
 
+
+@app.route("/health")
+def health():
+    with open("healthcheck_flag.txt", "r") as f:
+        try:
+            healthcheck_flag = int(f.readline())
+        except:
+            healthcheck_flag = False
+
+    if healthcheck_flag:
+        return "Ok", 200
+    else:
+        return "", 503
+
+
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def catch_all(path):
