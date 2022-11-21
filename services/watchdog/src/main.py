05976cac4fc85a414aa943c73cc18789316da1b7
Eugene Taranov <1123643+eugenetaranov@users.noreply.github.com>
2022-11-21 16:31:00 +0300

    added requests

--- services/watchdog/src/main.py
+++ services/watchdog/src/main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+import requests
 from loguru import logger
 from time import sleep
 
+ENDPOINT = "http://alice-jobs-79468b8744-pjxbh:8123"
 
 while True:
     logger.debug("Test")
-    sleep(10)
+    requests.get(ENDPOINT)
+    logger.debug(requests.text)
+    sleep(30)
