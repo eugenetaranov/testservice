0fffe5a610357d9f714e70be81da610dbbd300f8
Eugene Taranov <1123643+eugenetaranov@users.noreply.github.com>
2022-11-21 18:12:45 +0300

    test

--- services/watchdog/src/main.py
+++ services/watchdog/src/main.py
@@ -9,6 +9,20 @@ from yaml.loader import SafeLoader
 from time import sleep
 
 
+class K8S:
+    def __init__(self):
+        config.load_incluster_config()
+        self.v1 = client.CoreV1Api()
+
+    def get_pods(self, namespaces=[]) -> object:
+        pods = []
+        for namespace in namespaces:
+            res = self.v1.list_namespaced_pod(namespace=namespace, watch=False)
+            pods.extend(res.items)
+        
+        return pods
+
+
 def main():
     config_path = os.getenv("CONFIG_FILE")
     with open(config_path) as f:
@@ -16,15 +30,18 @@ def main():
     
     logger.debug(cfg)
 
-    config.load_incluster_config()
-    v1 = client.CoreV1Api()
+    # config.load_incluster_config()
+    # v1 = client.CoreV1Api()
+
+    k = K8S()
 
     while True:
-        ret = v1.list_pod_for_all_namespaces(watch=False)
-        for i in ret.items:
+        pods = k.get_pods(namespaces=cfg["namespaces"])
+        for i in pods:
             logger.info(f"{i.metadata.namespace}/{i.metadata.name} {i.status.pod_ip}")
 
         sleep(60)
+
 
 if __name__ == '__main__':
     main()
