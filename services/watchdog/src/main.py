47f03c3cda2ddc86aa759e260c1cd432145afb98
Eugene Taranov <1123643+eugenetaranov@users.noreply.github.com>
2022-11-21 16:51:49 +0300

    test

--- services/watchdog/src/main.py
+++ services/watchdog/src/main.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python
 
 import requests
+from kubernetes import client, config
 from loguru import logger
 from time import sleep
 
 POD_NAME = "alice-jobs-79468b8744-pjxbh"
 
-while True:
-    ENDPOINT = f"http://{POD_NAME}:8123"
-    logger.debug("Test")
 
-    try:
-        requests.get(ENDPOINT, timeout=1)
-    except requests.exceptions.Timeout:
-        logger.error(f"Timed out query to {POD_NAME}")
-        sleep(30)
-        continue
-    
-    logger.debug(requests.text)
-    sleep(30)
+def main():
+    config.load_incluster_config()
+    v1 = client.CoreV1Api()
+
+    while True:
+        ret = v1.list_pod_for_all_namespaces(watch=False)
+        for i in ret.items:
+            logger.info(f"{i.metadata.namespace}/{i.metadata.name} {i.status.pod_ip}")
+
+        sleep(60)
+
+if __name__ == '__main__':
+    main()
+
+# while True:
+#     ENDPOINT = f"http://{POD_NAME}:8123"
+
+#     try:
+#         requests.get(ENDPOINT, timeout=1)
+#     except requests.exceptions.Timeout:
+#         logger.error(f"Timed out query to {POD_NAME}")
+#         sleep(30)
+#         continue
+
+#     logger.debug(requests.text)
+#     sleep(30)
