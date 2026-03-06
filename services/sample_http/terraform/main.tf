aa70e534264282c586e18d06487d54b1a6f5e306
Eugene Taranov <1123643+eugenetaranov@users.noreply.github.com>
2026-03-06 10:13:37 +0100

    Fix sticky cookie annotation key for Traefik

--- services/sample_http/terraform/main.tf
+++ services/sample_http/terraform/main.tf
@@ -120,7 +120,7 @@ resource "kubernetes_service_v1" "testservice" {
     annotations = {
       "traefik.ingress.kubernetes.io/service.sticky.cookie"        = var.stickiness_enabled ? "true" : "false"
       "traefik.ingress.kubernetes.io/service.sticky.cookie.name"   = var.stickiness_enabled ? "testservice" : ""
-      "traefik.ingress.kubernetes.io/service.sticky.cookie.maxAge" = var.stickiness_enabled ? "30" : ""
+      "traefik.ingress.kubernetes.io/service.sticky.cookie.maxage" = var.stickiness_enabled ? "30" : ""
     }
   }
 
